b180d5103cc14c63975c54cacb6f72361564e1b9
kamabokochan <caffe@harachan.local>
2020-12-06 21:55:52 +0900

    カメラ切り替え機能追加

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+  [SerializeField] GameObject[] panels;
+  int currentPanelIndex = 0;
+  void Start() {
+    // 0番目のパネルを初期表示とする
+    panels[currentPanelIndex].SetActive(true);
+  }
+
+  // 回転する前に一度全てのパネルを非表示にする（初期化する）
+  public void InactivePanel () {
+    for (int i  = 0; i < panels.Length; i++) {
+      panels[i].SetActive(false);
+    }
+  }
+
+  // 左回転の処理
+  public void TurnLeft() {
+    InactivePanel();
+    currentPanelIndex--;
+    if (currentPanelIndex < 0) {
+      currentPanelIndex = panels.Length -1;
+    }
+    panels[currentPanelIndex].SetActive(true);
+  }
+
+  // 右回転の処理
+  public void TurnRight() {
+    InactivePanel();
+    currentPanelIndex++;
+    if (currentPanelIndex >= panels.Length) {
+      currentPanelIndex = 0;
+    }
+    panels[currentPanelIndex].SetActive(true);
+  }
+}
