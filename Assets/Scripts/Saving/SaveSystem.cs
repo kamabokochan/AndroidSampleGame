315392c57a93d45115a45996f542593a8a1ff1d1
kamabokochan <h.sasaki.worksfront@gmail.com>
2021-01-01 19:58:21 +0900

    セーブ機能の実装テスト

---
+++ Assets/Scripts/Saving/SaveSystem.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class SaveSystem
+{
+    #region Singleton
+    public static SaveSystem instance = new SaveSystem();
+    public static SaveSystem CombineInstance => instance;
+    #endregion
+
+  public SaveSystem() { }
+  public string Path => Application.dataPath + "/data.json";
+  // private UserData userData = new UserData();
+
+  public UserData UserData { get; private set; }
+
+  public void Save()
+  {
+    string jsonData = JsonUtility.ToJson(UserData);
+    // true: 追記, false: 上書き
+    StreamWriter writer = new StreamWriter(Path, false);
+    writer.WriteLine(jsonData);
+    writer.Flush();
+    writer.Close();
+    Debug.Log("セーブしました");
+  }
+
+  public void Load()
+  {
+    if (!File.Exists(Path))
+    {
+      Debug.Log("初回起動");
+      UserData = new UserData();
+      Save();
+      return;
+    }
+
+    StreamReader reader = new StreamReader(Path);
+    string jsonData = reader.ReadToEnd();
+    UserData = JsonUtility.FromJson<UserData>(jsonData);
+    reader.Close();
+  }
+}
---
+++ Assets/Scripts/Saving/SaveSystemMono.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveSystemMono : MonoBehaviour
+{
+  public void Save()
+  {
+    SaveSystem.instance.UserData.UserName = "aaa";
+    SaveSystem.instance.Save();
+  }
+
+  public void Load()
+  {
+    SaveSystem.instance.Load();
+  }
+}
